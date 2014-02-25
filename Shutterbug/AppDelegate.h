0d0e7cb3f9041d9b6f45aa318f49ab48ff6ceabf
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-02-25 16:47:43 -0300

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Denis C de Azevedo on 25/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Denis C de Azevedo on 25/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
